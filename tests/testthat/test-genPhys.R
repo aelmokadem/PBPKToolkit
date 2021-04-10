a5057fa12bdfd1ee3fdbe9c3ef206f84974e069b
Ahmed Elmokadem <ahmede@Ahmedes-MacBook-16-Pro.local>
2021-04-10 02:53:31 -0400

    update genPhys test

--- tests/testthat/test-genPhys.R
+++ tests/testthat/test-genPhys.R
@@ -14,6 +14,20 @@ test_that("genInd", {
   )
 })
 
+test_that("genInd_error_outOfRange", {
+  expect_error(
+    genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.1),
+    regexp = "Target height and BMI are out of range for the chosen age and sex"
+  )
+})
+
+test_that("genInd_error_inputs", {
+  expect_error(
+    genInd(age=30, is.male=TRUE, bw_targ=73),
+    regexp = "At least two inputs of bw_targ, ht_targ, or bmi_targ are required"
+  )
+})
+
 ## genPop
 ref <- dget(file.path(out_dir, "genPop"))
 
@@ -24,5 +38,12 @@ test_that("genInd", {
   expect_equal(
     res,
     ref
+  )
+})
+
+test_that("genPop_error_inputs", {
+  expect_error(
+    genPop(nSubj=2, minAge=20, maxAge=80, femPerc=50, minBW=50, maxBW=100, minHT=1.5),
+    regexp = "Minimum and maximum values for height are required"
   )
 })
