27890c1311514fb99ae32fa2b9d21355e63d478d
ahmede <ahmede@metrumrg.com>
2022-05-03 22:15:50 -0400

    introduce huisinga method and tests

--- data-raw/render-genPhys-refs.R
+++ data-raw/render-genPhys-refs.R
@@ -2,20 +2,35 @@ out_dir <- "genPhysRefs"
 out_dir <- file.path(system.file("test-refs", package = "PBPKToolkit"), out_dir)
 if(!fs::dir_exists(out_dir)) fs::dir_create(out_dir)
 
-## genInd with adding BC
+## genInd with adding BC for Willmann method
 set.seed(123)
-res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, optimize = TRUE)
-dput(res, file.path(out_dir, "genInd_BC"))
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, optimize = TRUE, method="Willmann")
+dput(res, file.path(out_dir, "genInd_BC_willmann"))
 
-## genInd without BC
+## genInd without BC for Willmann method
 set.seed(123)
-res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, optimize = TRUE, addBC=FALSE)
-dput(res, file.path(out_dir, "genInd_noBC"))
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, optimize = TRUE, addBC=FALSE, method="Willmann")
+dput(res, file.path(out_dir, "genInd_noBC_willmann"))
 
-## genPop
+## genInd with adding BC for Huisinga method
 set.seed(123)
-res <- genPop(nSubj=2, minAge=20, maxAge=80, femPerc=50, minBW=50, maxBW=100, minHT=1.5, maxHT=1.9, optimize=FALSE)
-dput(res, file.path(out_dir, "genPop"))
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, optimize = TRUE, method="Huisinga")
+dput(res, file.path(out_dir, "genInd_BC_huisinga"))
+
+## genInd without BC for Huisinga method
+set.seed(123)
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, optimize = TRUE, addBC=FALSE, method="Huisinga")
+dput(res, file.path(out_dir, "genInd_noBC_huisinga"))
+
+## genPop for Willmann
+set.seed(123)
+res <- genPop(nSubj=2, minAge=20, maxAge=80, femPerc=50, minBW=50, maxBW=100, minHT=1.5, maxHT=1.9, optimize=FALSE, method="Willmann")
+dput(res, file.path(out_dir, "genPop_willmann"))
+
+## genPop for Huisinga
+set.seed(123)
+res <- genPop(nSubj=2, minAge=20, maxAge=80, femPerc=50, minBW=50, maxBW=100, minHT=1.5, maxHT=1.9, optimize=FALSE, method="Huisinga")
+dput(res, file.path(out_dir, "genPop_huisinga"))
 
 ## genInd_mab
 set.seed(123)
--- tests/testthat/test-genPhys.R
+++ tests/testthat/test-genPhys.R
@@ -1,11 +1,50 @@
 out_dir <- "genPhysRefs"
 out_dir <- file.path(system.file("test-refs", package = "PBPKToolkit"), out_dir)
 
-## genInd
-ref <- dget(file.path(out_dir, "genInd"))
+## genInd_BC for Willmann
+ref <- dget(file.path(out_dir, "genInd_BC_willmann"))
 
 set.seed(123)
-res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, optimize = TRUE)
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, addBC=TRUE, optimize = TRUE, method="Willmann")
+
+test_that("genInd", {
+  expect_equal(
+    res,
+    ref
+  )
+})
+
+## genInd_noBC for Willmann
+ref <- dget(file.path(out_dir, "genInd_noBC_willmann"))
+
+set.seed(123)
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, addBC=FALSE, optimize = TRUE, method="Willmann")
+
+test_that("genInd", {
+  expect_equal(
+    res,
+    ref
+  )
+})
+
+## genInd_BC for Huisinga
+ref <- dget(file.path(out_dir, "genInd_BC_huisinga"))
+
+set.seed(123)
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, addBC=TRUE, method="Huisinga")
+
+test_that("genInd", {
+  expect_equal(
+    res,
+    ref
+  )
+})
+
+## genInd_noBC for Huisinga
+ref <- dget(file.path(out_dir, "genInd_noBC_huisinga"))
+
+set.seed(123)
+res <- genInd(age=30, is.male=TRUE, bw_targ=73, ht_targ=1.76, addBC=FALSE, method="Huisinga")
 
 test_that("genInd", {
   expect_equal(
@@ -28,11 +67,24 @@ test_that("genInd_error_inputs", {
   )
 })
 
-## genPop
-ref <- dget(file.path(out_dir, "genPop"))
+## genPop for Willmann
+ref <- dget(file.path(out_dir, "genPop_willmann"))
 
 set.seed(123)
-res <- genPop(nSubj=2, minAge=20, maxAge=80, femPerc=50, minBW=50, maxBW=100, minHT=1.5, maxHT=1.9, optimize=FALSE)
+res <- genPop(nSubj=2, minAge=20, maxAge=80, femPerc=50, minBW=50, maxBW=100, minHT=1.5, maxHT=1.9, optimize=FALSE, method="Willmann")
+
+test_that("genPop", {
+  expect_equal(
+    res,
+    ref
+  )
+})
+
+## genPop for Huisinga
+ref <- dget(file.path(out_dir, "genPop_huisinga"))
+
+set.seed(123)
+res <- genPop(nSubj=2, minAge=20, maxAge=80, femPerc=50, minBW=50, maxBW=100, minHT=1.5, maxHT=1.9, optimize=FALSE, method="Huisinga")
 
 test_that("genPop", {
   expect_equal(
